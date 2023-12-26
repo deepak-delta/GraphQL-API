094ba4d5864cf008f3761baf4c1c53fef9dae727
deepak-delta <ddk3857@gmail.com>
2023-12-26 20:36:05 +0530

    password hash middleware

--- src/Schema/user.schema.ts
+++ src/Schema/user.schema.ts
@@ -1,7 +1,20 @@
-import { getModelForClass, modelOptions, prop } from '@typegoose/typegoose'
+import { getModelForClass, modelOptions, pre, prop } from '@typegoose/typegoose'
 import { IsEmail, MinLength } from 'class-validator'
 import { Field, InputType, ObjectType } from 'type-graphql'
+import bcrypt from 'bcrypt'
 
+//Password hash middleware
+@pre<User>('save', async function () {
+  if (!this.isModified('password')) {
+    return
+  }
+
+  const salt = await bcrypt.genSalt(10)
+
+  const hash = await bcrypt.hashSync(this.password, salt)
+
+  this.password = hash
+})
 @ObjectType()
 @modelOptions({ schemaOptions: { collection: 'users' } })
 export class User {
